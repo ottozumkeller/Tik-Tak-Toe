c3e46c6bed0cb671ec3ab318712b8faf7c392155
ottozumkeller <94012287+ottozumkeller@users.noreply.github.com>
2023-08-13 20:21:33 +0200

    Update main.c

--- cr-build.bat
+++ cr-build.bat
@@ -1,13 +1,14 @@
 @echo off
 title Crinkler
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat" > nul
 
 cl /c /GS- /Gr /FC /O1 /fp:strict /MP /TC /Gy /Ob0 /Zc:inline /Zp8 /fp:except- /GF /MT /std:c++14 /Os main.c
 
-crinkler /tinyheader /priority:idle /subsystem:console /entry:main /ordertries:30000 /unaligncode /noinitializers /unsafeimport /out:tiktak_crinkler.exe /report:report.html main.obj kernel32.lib user32.lib
+crinkler /tinyheader /priority:idle /subsystem:console /entry:main /ordertries:30000 /unaligncode /noinitializers /unsafeimport /out:tiktak_crinkler_normal.exe /report:report_normal.html main.obj kernel32.lib user32.lib
+crinkler /tinyheader /tinyimport /priority:idle /subsystem:console /entry:main /ordertries:30000 /unaligncode /noinitializers /out:tiktak_crinkler_tiny.exe /report:report_tiny.html main.obj kernel32.lib user32.lib
 
-move tiktak_crinkler.exe Release\
-move report.html Reports\
+move tiktak_crinkler_*.exe Release\
+move report_*.html Reports\
 
 del main.obj
 pause
--- main.c
+++ main.c
@@ -3,11 +3,12 @@
 #include<stdio.h>
 #include<windows.h>
 #include<conio.h>
+#include<ctype.h>
 
 #define PLAYER1		'X' // player 1 mark (must be capital)
 #define PLAYER2		'O' // player 2 mark (must be capital)
-#define COLUMNS		23
-#define ROWS		11
+#define COLUMNS		19
+#define ROWS		9
 
 int end(char mrk[]) {
 	int end = 1;
@@ -22,56 +23,71 @@ int end(char mrk[]) {
 }
 
 void print(char mrk[], char plyr_char) {
-	printf("\x1b[?25h\x1b[8;%d;%dt\x1b]2; \a\x1b[2J\x1b[%dH\x1b(0", ROWS, COLUMNS, (ROWS - 7) / 2 + 1);
-	char args[7][14] = { "lqqqwqqqwqqqk", "", "tqqqnqqqnqqqu", "", "tqqqnqqqnqqqu", "", "mqqqvqqqvqqqj" };
+	printf("\x1b[8;%d;%dt\x1b]2; \a\x1b[2J\x1b[%dH\x1b(0", ROWS, COLUMNS, (ROWS - 7) / 2 + 1);
+	char args[7][15] = {
+		"lqqqwqqqwqqqk\n",
+		"x # x # x # x\n",
+		"tqqqnqqqnqqqu\n",
+		"x # x # x # x\n",
+		"tqqqnqqqnqqqu\n",
+		"x # x # x # x\n",
+		"mqqqvqqqvqqqj\r"
+	};
+	int index = 0;
 	for (int i = 0; i < 7; i++) {
-		int j = (i + 1) / 3;
-		sprintf(args[2 * j + 1], "x %c x %c x %c x", mrk[3 * j], mrk[3 * j + 1], mrk[3 * j + 2]);
 		if (end(mrk)) {
-			sprintf(args[i], "\x1b[?25l");
-			sprintf(args[2], end(mrk) == 2 ? "%c WINS!" : "TIE!", plyr_char);
-			sprintf(args[4], " NEW GAME?\a");
+			sprintf(args[i], "\x1b[?25l\n");
+			sprintf(args[2], end(mrk) == 2 ? "%c WINS!\n" : "TIE!\n", plyr_char);
+			sprintf(args[4], "\aNEW GAME?\n");
+		} else {
+			for (int j = 0; j < 14; j++) {
+				if (args[i][j] == '#') {
+					args[i][j] = mrk[index++];
+				}
+			}
 		}
-		printf("\x1b[%dG%s\n", (COLUMNS - strlen(args[i])) / 2 + 1, args[i]);
+		printf("\x1b[%dG%s", (COLUMNS - strlen(args[i])) / 2 + 2, args[i]);
 	}
 }
 
 void main() {
-	srand(GetTickCount64());
-	int plyr = rand() % 2, x = (COLUMNS + 1) / 2, y = (ROWS + 1) / 2;
-	char mrk[9] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
 	SetWindowLongW(GetConsoleWindow(), GWL_STYLE, WS_VISIBLE | WS_CAPTION | WS_SYSMENU);
 	system("mode 13,1");
-	print(mrk, 0);
 	do {
-		printf("\x1b[%d;%dH", y, x);
-		switch (_getch()) {
-			case 224:
-				{
+		srand(GetTickCount64());
+		int plyr = rand() % 2, x = (COLUMNS + 1) / 2, y = (ROWS + 1) / 2;
+		char mrk[9] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+		do {
+			print(mrk, 0);
+			printf("\x1b[?25h\x1b[%d;%dH", y, x);
+			int c = _getch();
+			if (c == 224) {
 				int d = _getch() - 76;
 				if (d % 2) {
 					int nx = x + d * 4;
 					if (nx > (COLUMNS - 13) / 2 && nx < (COLUMNS - (COLUMNS - 13) / 2)) {
 						x = nx;
 					}
-				} else {
+				}
+				else {
 					int ny = y + d / 2;
 					if (ny > (ROWS - 7) / 2 && ny < (ROWS - (ROWS - 7) / 2)) {
 						y = ny;
 					}
 				}
-			}
-			break;
-			case 13:
-			{
+			} else if (c == 13) {
 				int pos = ((y - (ROWS - 7) / 2) / 3) * 3 + (x - (COLUMNS - 13) / 2) % 3;
 				if (mrk[pos] == ' ') {
 					print(mrk, mrk[pos] = PLAYER1 - !(plyr ^= !end(mrk)) * (PLAYER1 - PLAYER2));
 				}
+			} else if (isxdigit(c)) {
+				printf("%c\x1b[D", toupper(c));
+				int d = _getch();
+				if (isxdigit(d)) {
+					char hex[9] = {'c', 'o', 'l', 'o', 'r', ' ', c, d};
+					system(hex);
+				}
 			}
-			break;
-		}
-	} while (!end(mrk));
-	_getch();
-	main();
+		} while (!end(mrk));
+	} while (_getch());
 }
