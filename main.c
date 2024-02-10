491873a06b2715bc91c7fea6899fe5a115e4ed40
ottozumkeller <94012287+ottozumkeller@users.noreply.github.com>
2024-02-10 12:42:00 +0100

    Update main.c

--- main.c
+++ main.c
@@ -7,8 +7,8 @@
 
 #define PLAYER1 'O'
 #define PLAYER2 'X'
-#define COLUMNS 19
-#define ROWS    9
+
+int columns = 0, rows = 0, x = 0, y = 0, mrk[9] = { 0 }, mult = 0, plyr = 0;
 
 int end(int mrk[]) {
     int end = 1;
@@ -40,44 +40,68 @@ int minimax(int mrk[], int is_max) {
     return best;
 }
 
-void main() {
-    SetWindowLongW(GetConsoleWindow(), GWL_STYLE, WS_VISIBLE | WS_CAPTION | WS_SYSMENU);
-    int x = (COLUMNS + 1) / 2 - 3, y = (ROWS + 1) / 2, mrk[9] = { 0 }, mult = 0, plyr = 0;
-    while (1) {
-        if (end(mrk) || !plyr) {
-            x = (COLUMNS + 1) / 2 - (3 - 6 * mult);
-            y = (ROWS + 1) / 2;
-        }
-        printf("\33[8;%d;%dt", ROWS, COLUMNS);
-        SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), (COORD) { COLUMNS, ROWS });
-        printf("\33]2; \a\33[2J\33[%dH\33(0", (ROWS - 5) / 2);
-        char rows[7][15] = {
-            "lqqqwqqqwqqqk\n",
-            "x   x   x   x\n",
-            "tqqqnqqqnqqqu\n",
-            "x   x   x   x\n",
-            "tqqqnqqqnqqqu\n",
-            "x   x   x   x\n",
-            "mqqqvqqqvqqqj\r"
-        };
-        int index = 0;
-        for (int i = 0; i < 7; i++) {
-            static const int plyr_arr[3] = { ' ', PLAYER1, PLAYER2 };
-            if (!end(mrk) ^ !plyr) {
-                for (int j = 2; j < 11; j++) {
-                    if (rows[i][j - 2] == 'x') {
-                        rows[i][j] = plyr_arr[mrk[index++]];
-                    }
+void draw() {
+    char cmd[16];
+    sprintf(cmd, "mode %d,%d", columns, rows);
+    system(cmd);
+    printf("\33[2J\33]8;%d;%dt\33]0; \a\33[%dH\33(0", columns, rows, (rows - 5) / 2);
+    char lines[7][15] = {
+        "lqqqwqqqwqqqk\n",
+        "x   x   x   x\n",
+        "tqqqnqqqnqqqu\n",
+        "x   x   x   x\n",
+        "tqqqnqqqnqqqu\n",
+        "x   x   x   x\n",
+        "mqqqvqqqvqqqj\r"
+    };
+    int index = 0;
+    for (int i = 0; i < 7; i++) {
+        static const int plyr_arr[3] = { ' ', PLAYER1, PLAYER2 };
+        if (!end(mrk) ^ !plyr) {
+            for (int j = 2; j < 11; j++) {
+                if (lines[i][j - 2] == 'x') {
+                    lines[i][j] = plyr_arr[mrk[index++]];
                 }
-            } else {
-                sprintf(rows[i], "\n");
-                sprintf(rows[1], !plyr ? " SELECT MODE \n" : (end(mrk) == 2 ? "   %c WINS!  \n" : "     TIE     \n"), plyr_arr[plyr]);
-                sprintf(rows[3], "  [1]   [2]  \n");
-                sprintf(rows[5], "  PLAYER(S)  \r");
             }
-            printf("\33[%dG%s", (COLUMNS - 11) / 2, rows[i]);
         }
-        printf("\33[%d;%dH", y, x);
+        else {
+            sprintf(lines[i], "\n");
+            sprintf(lines[1], !plyr ? " SELECT MODE \n" : (end(mrk) == 2 ? "   %c WINS!  \n" : "     TIE!    \n"), plyr_arr[plyr]);
+            sprintf(lines[3], "  [1]   [2]  \n");
+            sprintf(lines[5], "  PLAYER(S)  \r");
+        }
+        printf("\33[%dG%s", (columns - 11) / 2, lines[i]);
+    }
+    if (end(mrk) || !plyr) {
+        x = (columns + 1) / 2 - (3 - 6 * mult);
+        y = (rows + 1) / 2;
+    }
+    printf("\33[%d;%dH", y, x);
+}
+
+DWORD WINAPI update_size() {
+    CONSOLE_SCREEN_BUFFER_INFO info;
+    while (1) {
+        GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
+        int ncolumns = info.srWindow.Right - info.srWindow.Left + 1;
+        int nrows = info.srWindow.Bottom - info.srWindow.Top + 1;
+        if (abs(columns - ncolumns) > 2 || ncolumns < 15 || abs(rows - nrows) > 1 || nrows < 7) {
+            ncolumns = ncolumns < 15 ? 15 : ncolumns;
+            nrows = nrows < 7 ? 7 : nrows;
+            x = (x - (columns + 1) / 2) + (ncolumns + 1) / 2;
+            y = (y - (rows + 1) / 2) + (nrows + 1) / 2;
+            columns = ncolumns;
+            rows = nrows;
+            draw();
+        }
+    }
+}
+
+void main() {
+    SetWindowLongW(GetConsoleWindow(), GWL_STYLE, WS_VISIBLE | WS_SIZEBOX | WS_CAPTION | WS_SYSMENU);
+    CreateThread(NULL, 0, update_size, NULL, NULL, NULL);
+    while (1) {
+        draw();
         const int inp = end(mrk) || mult || plyr != 2 ? _getch() : 0;
         if (!end(mrk) && plyr) {
             if (!mult && plyr == 2) {
@@ -101,14 +125,14 @@ void main() {
                     int d = _getch() - 76;
                     if (d % 2) {
                         d *= 4;
-                        if (abs(x - (COLUMNS + 1) / 2 + d) < 5) x += d; // for non wrapping cursor change "d * -8" to "0"
+                        if (abs(x - (columns + 1) / 2 + d) < 5) x += d;
                     } else {
                         d /= 2;
-                        if (abs(y - (ROWS + 1) / 2 + d) < 3) y += d; // for non wrapping cursor change "-d" to "0"
+                        if (abs(y - (rows + 1) / 2 + d) < 3) y += d;
                     }
                     break;
                 case 13:;
-                    const int pos = ((y - (ROWS - 7) / 2) / 3) * 3 + (x - (COLUMNS - 13) / 2) % 3;
+                    const int pos = ((y - (rows - 7) / 2) / 3) * 3 + (x - (columns - 13) / 2) % 3;
                     if (!mrk[pos]) {
                         plyr = (((mrk[pos] = plyr) - 1) ^ !end(mrk)) + 1;
                     }
@@ -120,10 +144,9 @@ void main() {
                 mult ^= _getch() % 2;
                 break;
             case 13:;
-                for (int i = 0; i < 9; i++) {
-                    mrk[i] = 0;
-                }
-                x = (COLUMNS + 1) / 2;
+                memset(mrk, 0, 36);
+                x = (columns + 1) / 2;
+                y = (rows + 1) / 2;
                 srand(GetTickCount64());
                 plyr = mult ? rand() % 2 + 1 : 1;
             }
